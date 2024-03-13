4050e3b5789e5dab97f5880919f2dcb8791058dd
DarwinL14 <yulieth.astaiza@soy.sena.edu.co>
2024-03-13 17:30:15 -0500

    formularios

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from .models import Medico
 from .models import Paciente
 from .models import Consultorio
 from .models import Cita
-from flask import render_template, request
+from flask import render_template, request, flash, redirect
 
 #crear ruta para ver los medicos
 @app.route("/medicos")
@@ -84,7 +84,32 @@ def create_medico():
         
         db.session.add(new_medico)
         db.session.commit()
-        return "medico registrado"
+        flash("Medico registrado correctamente")
+        return redirect("/medicos")
+    
+@app.route("/medicos/update/<int:id>", methods=["POST","GET"])
+def update_medico(id):
+    especialidades = ["cardiologia", "psicologia", "dermatologia"]
+    medico_update = Medico.query.get(id)
+    if(request.method =="GET"):
+        return render_template("medico_update.html" , medico_update = medico_update, especialidades = especialidades)
+    elif(request.method == "POST"):
+        #ACTUALIZAR EL MEDICO EN DATOS DE FORMA
+        medico_update.nombre = request.form["nombre"]
+        medico_update.apellido = request.form["apellidos"]
+        medico_update.tipo_identificacion = request.form["ti"]
+        medico_update.numero_identificacion = request.form["ni"]
+        medico_update.registro_medico = request.form["rm"]
+        medico_update.especialidad = request.form["es"]
+        db.session.commit()
+        return "medico actualizado"
+    
+@app.route("/medicos/delete/<int:id>")
+def delete_medico(id):
+    medico_delete = Medico.query.get(id)
+    db.session.delete(medico_delete)
+    db.session.commit()
+    return redirect("/medicos")
 
 #NUEVA RUTA PARA PACIENTES
 @app.route("/pacientes/create" , methods = [ "GET" , "POST"] )
@@ -94,7 +119,7 @@ def create_paciente():
             "A+",
             "A-",
             "B+",
-            "B-"
+            "B-",
             "O+",
             "O-",
             "AB+",
